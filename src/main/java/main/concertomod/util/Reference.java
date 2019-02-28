4c3a7a5f5dbbe81780069b6a557e7af4aafeacc7
AlphaSerpentis <arandomserpentis@gmail.com>
2019-02-27 19:57:28 -0600

    version

--- src/main/java/main/concertomod/util/Reference.java
+++ src/main/java/main/concertomod/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "cm"; //Concerto Mod = cm
 	public static final String NAME = "Concerto Mod";
-	public static final String VERSION = "0.01";
+	public static final String VERSION = "pre-0.01";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "main.concertomod.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "main.concertomod.proxy.CommonProxy";
